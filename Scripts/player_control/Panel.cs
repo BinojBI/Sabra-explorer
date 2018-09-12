cb7cfa186e232f08d833f6f50b398d9392b0ef97
BinojBI <binojishara21@gmail.com>
2018-09-13 00:53:46 +0530

    player controller scripts added

---
+++ Scripts/player_control/Panel.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panel : MonoBehaviour {
+    //palyer_controller
+    public GameObject uiObject;
+    public GameObject uiObject2;
+    private Animator anim;
+    public bool display = false;
+    public bool animate;
+   
+    // Use this for initialization
+    void Start()
+    {
+       uiObject.SetActive(false);
+        //anim.SetBool("open_gate", false);
+        anim = GetComponent<Animator>();
+       
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (uiObject.active == true && Input.GetKeyDown(KeyCode.E))
+        {
+            anim.Play("open_gate");
+            uiObject.SetActive(false);
+
+        }
+    }
+
+    void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            uiObject.SetActive(true);
+            
+        }
+       
+
+
+    }
+    void OnTriggerExit(Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            uiObject.SetActive(false);
+        }
+
+    }
+   
+}
---
+++ Scripts/player_control/player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+    //palyer_controller
+    private Animator anim;
+    private float pSpeed;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        pSpeed = Input.GetAxis("Vertical");
+        anim.SetFloat("speed", pSpeed);
+	}
+}
