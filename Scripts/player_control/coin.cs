c20711c8eb525aef3f9b7aebc6dfcb490b442423
BinojBI <binojishara21@gmail.com>
2018-09-21 01:21:00 +0530

    coin objectives

--- Scripts/player_control/coin.cs
+++ Scripts/player_control/coin.cs
@@ -1,25 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class coin : MonoBehaviour {
-   
-	// Use this for initialization
-	void Start () {
-		
+
+    public Text guitxt;
+
+    // Use this for initialization
+    void Start () {
+        SetCount();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+      
+            }
 
-    void OnTriggerExit(Collider col) //If you exit the trigger this will happen.
+    void OnTriggerEnter(Collider col) //If you exit the trigger this will happen.
     {
         if (col.gameObject.tag == "Player")
         {
             Destroy(gameObject);
+            points.coinCollect();
+            Debug.Log(points.display());
+            SetCount();
+
         }
 
+    }
+
+    void SetCount()
+    {
+        guitxt.text = "Count : " + points.display();
     }
 }
---
+++ Scripts/player_control/lightPlace.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+    public class lightPlace : MonoBehaviour
+    {
+        public GameObject text_startGame;
+        public GameObject point_table;
+        public GameObject coin1;
+        
+
+
+    // Use this for initialization
+    void Start()
+        {
+            text_startGame.SetActive(false);
+            point_table.SetActive(false);
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+
+                text_startGame.SetActive(true);
+
+            }
+        }
+
+
+        void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                text_startGame.SetActive(false);
+            }
+        }
+
+        public void OnTriggerStay(Collider other)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                point_table.SetActive(true);
+           
+            
+                Instantiate(coin1, new Vector3(2.16F, 0.59f, 3.33f), Quaternion.identity);
+                Instantiate(coin1, new Vector3(5F, 0.59f, 3.33f), Quaternion.identity);
+        }
+        }
+    
+}
