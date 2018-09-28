35d3fe6c6720004d9d706189be2843c1af31eae4
BinojBI <binojishara21@gmail.com>
2018-09-28 15:22:58 +0530

    inventory scripts

---
+++ Scripts/player_control/item.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class item : MonoBehaviour {
+    public Vector3 pickupPosition;
+    public Vector3 pickupRotation;
+    public bool settrig;
+   
+	// Use this for initialization
+	void Start () {
+        settrig = false;
+    }
+	
+	// Update is called once per frame
+	public void FixedUpdate() {
+        if (settrig == true)
+        {
+            transform.position = pickupPosition;
+        }
+        
+        //gameObject.transform.eulerAngles = pickupRotation;
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "player")
+        {
+            settrig = true;
+        }
+    }
+}
