84e8326c3ee628ea6453cbc333ee793ccbbfd453
BinojBI <binojishara21@gmail.com>
2018-10-07 02:57:07 +0530

    add essential scripts chapter 2

---
+++ Scripts/Points.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Points : MonoBehaviour {
+
+	// Use this for initialization
+public static int point = 0;
+
+public static int getMarks (){
+for(int x=1; x<16;x++){
+point += x;
+}
+return point;
+}
+}
---
+++ Scripts/Target.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+public float health = 10f;
+public GameObject distructableditem;
+public float impactForce = 30f;
+
+	// Use this for initialization
+	private void Update() {
+		for(int x=0; x<16;x++){
+			
+			
+		}
+	}
+	public void TakeDamage(float amount){
+		health -= amount;
+		if(health <= 0){
+			Die();
+		}
+	}
+	void Die(){
+		Destroy(gameObject);
+		ShowPoints();
+		Instantiate(distructableditem, transform.position, transform.rotation);
+	
+		//Destroy(distructableditem,5f);
+	}
+
+	void ShowPoints(){
+		
+		int marks = Points.getMarks();
+		Debug.Log(marks);
+	}
+}
