df3a46649bb342f8f9a44a8426965fafac6b1920
BinojBI <binojishara21@gmail.com>
2018-11-16 23:58:20 +0530

    cricket scene created

---
+++ Assets/matta/crckrtmatch/shotcollider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shotcollider : MonoBehaviour {
+	public bool ballentered = false;
+	// Use this for initialization
+
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("ball"))
+		{
+			ballentered = true;
+			
+		}
+		
+	}
+
+	private void OnTriggerExit(Collider other) {
+		if (other.gameObject.CompareTag("ball"))
+		{
+			ballentered = false;
+			
+		}
+	}
+}
